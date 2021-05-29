9122baee518f0d59a7258a050032f4692f79bba2
Nishant Aswani <nishantaswani@nyu.edu>
2021-05-29 15:04:08 +0400

    Update accel.ino

--- accel/accel.ino
+++ accel/accel.ino
@@ -1,6 +1,6 @@
 #define M5STACK_MPU6886
 #include <FastLED.h>
-#include <M5Stack.h>
+#include <M5Atom.h>
 
 float X = 0;
 float Y = 0;
@@ -10,13 +10,7 @@ void setup() {
   Serial.begin(115200);
 //  delay(1000);
   M5.begin();
-  M5.Power.begin();
   M5.IMU.Init();
-  
-  
-  
-  
-  
 }
 
 int cnt = 0;
